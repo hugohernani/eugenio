d6d175d687f710cda9c4e2243432cb875b318634
hhernanni <hhernanni@02860cec-b1c4-4a97-a9f5-b711f4636968>
2014-12-02 22:50:26 +0000

    Bruno changes in DataAccess.cs and Bruno script (SaveLoad.cs) to deal with saving user information on OS.

--- Assets/Resources/Login Resources/Scripts/DataAccess.cs
+++ Assets/Resources/Login Resources/Scripts/DataAccess.cs
@@ -5,30 +5,33 @@ using System.Collections;
 using System.Collections.Generic; 
 
 public class DataAccess: MonoBehaviour{
-
+	
 	readonly string NAME_FIELD = "nome";
 	readonly string PASSWORD_FIELD = "senha";
-
+	
 	private readonly string serverUriPath = "http://localhost:8000/";
 	private readonly string userAccessUriPath = "User/";
 	private readonly string foodAccessUriPath = "Food/";
 	private readonly string TaskManagerAccessUriPath = "TaskManager/";
 	private readonly string GameAccessUriPath = "Game/";
 	private readonly string PetAccessUriPath = "VirtualPet/";
-
+	
 	private User user;
 	private WWW wwwResquest;
-
+	
 	public static bool FINISH = false;
-
+	
 	bool nextTaskAvailable = true;
-
+	
 	private static int previousTaskId = -1;
-
+	
+	SaveLoad saveLoad;
+	
 	void Awake(){
 		user = User.getInstance;
+		saveLoad = new SaveLoad(user.Name);
 	}
-
+	
 	public void POST(string url, Dictionary<string,string> post)
 	{
 		WWWForm form = new WWWForm();
@@ -40,7 +43,7 @@ public class DataAccess: MonoBehaviour{
 		
 		WaitForRequest(www);
 	}
-
+	
 	public bool POSTConfirmation(string url, Dictionary<string, string> post)
 	{
 		WWWForm form = new WWWForm ();
@@ -49,36 +52,36 @@ public class DataAccess: MonoBehaviour{
 			form.AddField(post_arg.Key, post_arg.Value);
 		}
 		WWW www = new WWW(url, form);
-
+		
 		return WaitForRequestWithConfirmation (www);
 	}
-
+	
 	public void GET(string url){
 		wwwResquest = new WWW (url);
-
+		
 		WaitForRequest (wwwResquest);
 	}
-
-
+	
+	
 	public void GET(string url, string getArgument){
 		wwwResquest = new WWW (url + "/" + getArgument);
-
+		
 		WaitForRequest (wwwResquest);
 	}
-
+	
 	private IEnumerator ILoginUser(string username, string password, Action<string> info){
 		string targetUri = serverUriPath + userAccessUriPath;
 		targetUri += "getUser/";
 		
 		Dictionary<string, string> userDict = new Dictionary<string, string> ();
-
+		
 		userDict.Add (NAME_FIELD, username);
 		userDict.Add (PASSWORD_FIELD, password);
-
+		
 		POST (targetUri, userDict);
-
+		
 		string response = wwwResquest.text;
-
+		
 		if (response != "") {
 			string[] textsplited = response.Split (',');
 			
@@ -107,24 +110,24 @@ public class DataAccess: MonoBehaviour{
 			info("Categorias de tarefas carregadas.");
 			yield return new WaitForSeconds(1f);
 			info("Tarefas carregadas.");
-
+			
 			yield break;
-
+			
 		} else {
-
+			
 			yield break;
 		}
-
-
-
+		
+		
+		
 	}
-
-
+	
+	
 	// TODO Until now just verify the connection with the server.
 	IEnumerator testConnection (Action<string> action, Action<bool> result)
 	{
 		bool availability;
-
+		
 		switch(Application.internetReachability){
 		case NetworkReachability.ReachableViaLocalAreaNetwork:
 			availability = true;
@@ -138,7 +141,7 @@ public class DataAccess: MonoBehaviour{
 		default:
 			availability = false;
 			break;
-
+			
 		}
 		if(availability){
 			WWW wwwTest = new WWW(serverUriPath); // server url
@@ -154,13 +157,13 @@ public class DataAccess: MonoBehaviour{
 		}else{
 			action("Sem conexao.");
 		}
-
+		
 		yield return null;
-
+		
 	}
-
+	
 	public IEnumerator LoginUser(string username, string password, Action<string> action){
-
+		
 		action("Verificando conexao");
 		bool result = false;
 		yield return StartCoroutine (testConnection (action, (connected) =>{
@@ -174,16 +177,16 @@ public class DataAccess: MonoBehaviour{
 			yield return new WaitForSeconds(1f);
 			action ("finished");
 		}
-
+		
 	}
-
+	
 	private void setAvailableFood(int level){
 		string targetUri = serverUriPath + foodAccessUriPath;
 		targetUri += ("filter/" + level + "/");
-
+		
 		GET(targetUri);
 		string response = wwwResquest.text;
-
+		
 		if(response != ""){
 			List<Dictionary<string, string>> listFoods = new List<Dictionary<string, string>>();
 			
@@ -199,48 +202,46 @@ public class DataAccess: MonoBehaviour{
 			user.AvailableFoods = listFoods;
 		}
 	}
-
+	
 	private void setPetStatus(){
 		string targetUri = serverUriPath + PetAccessUriPath;
 		targetUri += ("get/" + user.Id.ToString() + "/");
-
+		
 		GET (targetUri);
 		string response = wwwResquest.text;
-
+		
 		if(response != ""){
 			string[] statusObjects = response.Split(',');
-
+			
 			Debug.Log("Pet status: " + response);
-
+			
 			Pet petStatus = new Pet(
 				float.Parse(statusObjects[0]),
 				float.Parse(statusObjects[1]),
 				float.Parse(statusObjects[2])
 				);
 			petStatus.Experience = float.Parse(statusObjects[3]);
-
+			
 			user.CurrentPetStatus = petStatus;
-
+			
 		}
 	}
-
+	
 	public void updatePetStatus(float feed, float health, float entertainment, float experience){
 		string targetUri = serverUriPath + PetAccessUriPath;
 		targetUri += ("updatePet/" + user.Id.ToString() + "/" + feed.ToString() + "/" + health.ToString() +
 		              entertainment.ToString() + "/" + experience.ToString() + "/");
-
+		
 		GET (targetUri);
 		string response = wwwResquest.text;
-
+		
 		if (response == "1") {
 			Debug.Log ("Pet updated in the server side");
 		} else {
-			Debug.Log("A problem occured updating the server side");
-
-			// TODO Saving in file
+			Debug.Log("A problem occured updating the server side");	 
 		}
 	}
-
+	
 	public void updatePetStatus(Pet pet){
 		string targetUri = serverUriPath + PetAccessUriPath;
 		targetUri += ("updatePet/" + user.Id.ToString() + "/" + pet.Feed.ToString() + "/" + pet.Health.ToString() +
@@ -253,23 +254,25 @@ public class DataAccess: MonoBehaviour{
 			Debug.Log ("Pet updated in the server side");
 		} else {
 			Debug.Log("A problem occured updating the pet in the server side");
-
+			
+			saveLoad.SavePet(pet);
+			
 			// TODO Saving in file.
 		}
 	}
-
-
+	
+	
 	private void getAvailableGames(){
 		string targetUri = serverUriPath + GameAccessUriPath;
 		targetUri += "all/";
-
+		
 		GET (targetUri);
 		string response = wwwResquest.text;
-
+		
 		List<Game> games = new List<Game> ();
-
+		
 		if(response != ""){
-
+			
 			string[] gameInfos = response.Split(';');
 			foreach (string gameInfo in gameInfos) {
 				string[] values = gameInfo.Split('.');
@@ -279,19 +282,19 @@ public class DataAccess: MonoBehaviour{
 				games.Add(game);
 			}
 		}
-
+		
 		user.Games = games;
 	}
-
+	
 	private void retrieveCategories(){
 		string targetUri = serverUriPath + TaskManagerAccessUriPath;
 		targetUri += ("categories/all/" + user.Level_pet + "/" + user.CurrentStage + "/");
-
+		
 		GET (targetUri);
 		string response = wwwResquest.text;
-
+		
 		List<Category> categories = new List<Category> ();
-
+		
 		string[] categoryObjects = response.Split (';');
 		foreach(string sCategory in categoryObjects){
 			string[] values = sCategory.Split('.');
@@ -307,27 +310,27 @@ public class DataAccess: MonoBehaviour{
 				nextTaskAvailable = true;
 				category.SubCategories = retrieveSubCategories(category, category.Stage);
 			}
-
+			
 			categories.Add(category);
 		}
-
+		
 		user.Categories = categories;
 	}
-
+	
 	private List<Category> retrieveSubCategories(Category category, int categoryStage){
 		string targetUri = serverUriPath + TaskManagerAccessUriPath;
 		targetUri += ("subCategories/filter/" + category.Id + "/" + user.CurrentStage + "/" 
 		              + categoryStage + "/" + user.CurrentSubStage + "/");
-
+		
 		GET (targetUri);
 		string response = wwwResquest.text;
-
+		
 		if(response == ""){
 			retrieveTasksAndUserDoes(category, null);
 			return null; // There is not subcategory
 		}else{
 			List<Category> subCategories = new List<Category>();
-
+			
 			string[] subCategoryObjects = response.Split (';');
 			foreach(string subCategoryS in subCategoryObjects){
 				string[] values = subCategoryS.Split('.');
@@ -343,28 +346,28 @@ public class DataAccess: MonoBehaviour{
 					subCategory.Random = true;
 				subCategory.Available = bool.Parse(values[5]);
 				subCategories.Add(subCategory);
-
+				
 				retrieveTasksAndUserDoes(category, subCategory);
-
+				
 			}
-
+			
 			Debug.Log(subCategories.Count + " Subcategorias da categoria " + ((MainCategory) category).Name + ", " + category.Id + " adicionadas.");
-
+			
 			return subCategories;
 		}
 	}
-
+	
 	private void retrieveTasksAndUserDoes(Category category, Category subCategory){
 		string targetUri = serverUriPath + TaskManagerAccessUriPath;
 		targetUri += ("tasks/filter/" + user.Id + "/" + category.Id + "/");
-
+		
 		if (subCategory != null){
 			targetUri += (subCategory.Id + "/");
 		}
-
+		
 		GET (targetUri);
 		string response = wwwResquest.text;
-
+		
 		string[] taskObjects = response.Split (';');
 		foreach(string taskObject in taskObjects){
 			string[] values = taskObject.Split('*');
@@ -376,12 +379,12 @@ public class DataAccess: MonoBehaviour{
 				task.SubCategoryId = int.Parse(values[4]);
 			else
 				task.SubCategoryId = 0;
-
+			
 			user.AddTaskByVerification(task);
-
+			
 			if(values[5] != "0"){
 				task.Available = (nextTaskAvailable = true);
-
+				
 				string[] udValues = values[5].Split('|');
 				Debug.Log(values[5]);
 				User.UserDoes ud = new User.UserDoes();
@@ -393,47 +396,47 @@ public class DataAccess: MonoBehaviour{
 				ud.Duration = float.Parse(udValues[5]);
 				ud.Date_user_did = DateTime.ParseExact(udValues[6], "yyyy-MM-dd HH:mm:ss", null);
 				ud.Tentativa = int.Parse(udValues[7]);
-
-
+				
+				
 				user.AddUserDoesByVerification(ud);
 			}else{
 				Debug.Log("Guaranteed: " + nextTaskAvailable);
 				task.Available = nextTaskAvailable;
 				nextTaskAvailable = false;
-
+				
 			}
-
+			
 		}
-
+		
 	}
-
+	
 	void retrieveUserDoes(int task_id){
 		if (previousTaskId >= task_id)
 			return;
-
+		
 		previousTaskId = task_id;
-
+		
 		string targetUri = serverUriPath + userAccessUriPath;
 		targetUri += ("retrieveUserDoes/");
-
+		
 		Dictionary<string, string> postDict = new Dictionary<string, string> ();
-
+		
 		postDict.Add("user_id", user.Id.ToString());
 		postDict.Add("task_id", task_id.ToString());
-
+		
 		POST (targetUri, postDict);
-
+		
 		List<User.UserDoes> tempListUserDoes = new List<User.UserDoes> ();
-
+		
 		string response = wwwResquest.text;
-
+		
 		if (response == "")
 			return;
-
+		
 		string[] userDoesSList = response.Split(';');
 		foreach (string userDoesS in userDoesSList) {
 			string[] values = userDoesS.Split('.');
-
+			
 			User.UserDoes ud = new User.UserDoes();
 			ud.Id = int.Parse(values[0]);
 			ud.Hits = int.Parse(values[1]);
@@ -442,57 +445,59 @@ public class DataAccess: MonoBehaviour{
 			ud.Tentativa = int.Parse(values[4]);
 			ud.TaskId = int.Parse(values[5]);
 			ud.UserId = int.Parse(values[6]);
-
+			
 			tempListUserDoes.Add(ud);
-
+			
 			Debug.Log("Added a relational userTask");
 		}
-
+		
 		user.UserDoesList = tempListUserDoes;
 		user.CachedUserDoesList = tempListUserDoes;
-
+		
 	}
-
+	
 	public void createUpdateUserDoes(){
 		string targetUri = serverUriPath + userAccessUriPath;
 		targetUri += ("createUpdateUserDoes/");
-
+		
 		List<User.UserDoes> itemsToRemoveFromList = new List<User.UserDoes> ();
-
+		
 		foreach (User.UserDoes userDoes in user.CachedUserDoesList) {
 			Dictionary<string, string> userDoesDict = new Dictionary<string, string> ();
-
+			
 			userDoesDict.Add("user", userDoes.UserId.ToString());
 			userDoesDict.Add("task", userDoes.TaskId.ToString());
 			userDoesDict.Add("hits", userDoes.Hits.ToString());
 			userDoesDict.Add("stars", userDoes.Stars.ToString());
 			userDoesDict.Add("duration",userDoes.Duration.ToString());
 			userDoesDict.Add("date_user_did", userDoes.Date_user_did.ToString("yyyy-MM-dd HH:mm:ss"));
-
+			
 			bool result = POSTConfirmation(targetUri, userDoesDict);
-
+			
 			if(result){
 				Debug.Log("UserTask tuple created/updated in database");
-			}else{
+			} else {
 				// TODO save in file.
-
+				
+				saveLoad.SaveUserDoes(userDoes);
+				
 				Debug.Log("UserTask saved in file"); // TODO
 			}
 			itemsToRemoveFromList.Add(userDoes);
 		}
-
+		
 		foreach (User.UserDoes ud in itemsToRemoveFromList) {
 			user.RemoveUserTaskFromCachedList(ud);
 		}
-
+		
 	}
-
+	
 	public void updateUserInfo(){
 		string targetUri = serverUriPath + userAccessUriPath;
 		targetUri += ("update/");
-
+		
 		Dictionary<string, string> userDict = new Dictionary<string, string> ();
-
+		
 		userDict.Add("name", user.Name);
 		userDict.Add ("password", user.Password);
 		userDict.Add("level", user.Level_pet.ToString ());
@@ -500,68 +505,70 @@ public class DataAccess: MonoBehaviour{
 		userDict.Add("currentStage", user.CurrentStage.ToString());
 		userDict.Add("currentSubStage",user.CurrentSubStage.ToString());
 		userDict.Add("stars", user.Stars_qty.ToString());
-
+		
 		bool result = POSTConfirmation(targetUri, userDict);
 		
 		if(result){
-
+			
 			string response = wwwResquest.text;
-
+			
 			if (response == "1") {
 				Debug.Log ("User updated in the server side");
 			} else {
 				Debug.Log("A problem occured updating the User in the server side");
 				
+				saveLoad.SaveUserDict(userDict);
 				// TODO Saving in file due to problem in the server side
+				
 			}
 		}else{
 			// TODO save in file due to problem with conection.
+			saveLoad.SaveUserDict(userDict);
 			
 			Debug.Log("User saved in file"); // TODO
 		}
-
+		
 		updatePetStatus (user.CurrentPetStatus);
-
+		
 	}
-
-	private void WaitForRequest(WWW www)
-	{
-
+	
+	private void WaitForRequest(WWW www) {
+		
 		while(!www.isDone){
 			CheckDatabase.UriReach = www.url;
 		}
-
+		
 		wwwResquest = www;
-
+		
 		// check for errors
 		if (www.error == null)
 		{
 			Debug.Log("WWW Ok!: " + www.text);
-
+			
 			
 		} else {
 			Debug.Log("WWW Error: "+ www.error);
 		}    
 	}
-
-	private bool WaitForRequestWithConfirmation(WWW www){
-
+	
+	private bool WaitForRequestWithConfirmation(WWW www) {
+		
 		while(!www.isDone){
 		}
-
+		
 		wwwResquest = www;
-
+		
 		bool result = false;
-
+		
 		// check for errors
 		if (www.error == null)
 		{
 			result = true;
-
+			
 		} else{
 			result = false;
 		}
 		return result;
 	}
-
+	
 }
--- Assets/Resources/Login Resources/Scripts/SaveLoad.cs
+++ Assets/Resources/Login Resources/Scripts/SaveLoad.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
@@ -10,57 +9,70 @@ using System.IO;
 
 public class SaveLoad {
 
-	public List<User.UserDoes> savedUserDoes = new List<User.UserDoes>();
+	List<User.UserDoes> userDoesList;
+	
+	string userPath;
 
-	public List<Pet> savedPet = new List<Pet>();
+	public SaveLoad (string userName) {
+        userDoesList = new List<User.UserDoes>();
+		userPath = Application.persistentDataPath + "/" + userName;
+	}
 
-
-
-
-
-
-	public static void SaveUserDoes () {
-
-		SaveLoad.savedUserDoes.Add(User.UserDoes.current);
-
+	public void SaveUserDict (Dictionary<string, string> userDict) {
 		BinaryFormatter bf = new BinaryFormatter();
-		FileStream file = File.Create(Application.persistentDataPath + "/savedUserDoes.gd");
-		bf.Serialize(file, SaveLoad.savedUserDoes);
+		FileStream file = File.Create(userPath + "/savedUserDict.gd");
+		bf.Serialize(file, userDict);
 		file.Close();
 	}
 
-	public static void LoadUserDoes () {
-		
-		if(File.Exists(Application.persistentDataPath + "/savedUserDoes.gd")) { 
-			
+	public Dictionary<string, string> LoadUserDict () {
+		if(File.Exists(userPath + "/savedUserDict.gd")) { 
 			BinaryFormatter bf = new BinaryFormatter();
-			FileStream file = File.Open(Application.persistentDataPath + "/savedUserDoes.gd", FileMode.Open);
-			SaveLoad.savedUserDoes = (List<User.UserDoes>)bf.Deserialize(file);
+			FileStream file = File.Open(userPath + "/savedUserDict.gd", FileMode.Open);
+			Dictionary<string, string> userDict = (Dictionary<string, string>)bf.Deserialize(file);
 			file.Close();
-		}
+			
+			return userDict;
+		} else 
+			return null;
 	}
 
-
-	public void Save (Pet pet) {
-
+	public void SaveUserDoes (User.UserDoes userDoes) {
+		userDoesList.Add(userDoes);
 		BinaryFormatter bf = new BinaryFormatter();
-		FileStream file = File.Create(Application.persistentDataPath + "/savedPet.gd");
+		FileStream file = File.Create(userPath + "/savedUserDoesList.gd");
+		bf.Serialize(file, userDoesList);
+		file.Close();
+	}
+
+	public List<User.UserDoes> LoadUserDoesList () {
+		if(File.Exists(userPath + "/savedUserDoesList.gd")) { 
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(userPath + "/savedUserDoesList.gd", FileMode.Open);
+			userDoesList = (List<User.UserDoes>)bf.Deserialize(file);
+			file.Close();
+
+			return userDoesList;
+		} else 
+			return null;
+	}
+
+	public void SavePet (Pet pet) {
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(userPath + "/savedPet.gd");
 		bf.Serialize(file, pet);
 		file.Close();
 	}
 
-
-
 	public Pet LoadPet () {
-		
-		if(File.Exists(Application.persistentDataPath + "/savedPet.gd")) { 
-			
+		if(File.Exists(userPath + "/savedPet.gd")) { 
 			BinaryFormatter bf = new BinaryFormatter();
-			FileStream file = File.Open(Application.persistentDataPath + "/savedPet.gd", FileMode.Open);
-			Pet pet = bf.Deserialize(file);
+			FileStream file = File.Open(userPath + "/savedPet.gd", FileMode.Open);
+			Pet pet = (Pet)bf.Deserialize(file);
 			file.Close();
 
-			return pet
-		}
+			return pet;
+		} else 
+			return null;
 	}
 }
